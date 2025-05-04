a12a2940fd835e8e10bb9dce1dc90c9581f1a968
angelesmagalilateana <magali.lateana@gmail.com>
2025-05-04 20:10:24 -0300

    Clase 6 js

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
-const toggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('nav-menu');
-
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+const productos = [
+    {
+      nombre: "Lámpara Colgante Minimalista",
+      precio: 38000,
+      imagen: "./img/lampara-techo.png",
+    },
+    {
+      nombre: "Lámpara de Techo Moderna",
+      precio: 100000,
+      imagen: "./img/lampara-techo-2.png",
+    },
+    {
+      nombre: "Vela Aromática de Jazmín",
+      precio: 18000,
+      imagen: "./img/vela-jazmin.png",
+    },
+    {
+      nombre: "Vela Aromática de Vainilla",
+      precio: 25000,
+      imagen: "./img/vela-vainilla.png",
+    },
+    {
+      nombre: "Planta de interior",
+      precio: 23000,
+      imagen: "./img/planta-interior.png",
+    },
+    {
+      nombre: "Cuadro Blanco y Negro",
+      precio: 27000,
+      imagen: "./img/cuadro-blanco-negro.png",
+    },
+  ];
+  
+  const contenedorProductos = document.querySelector('.product-grid');
+  
+  productos.forEach(producto => {
+    const card = document.createElement('article');
+    card.classList.add('product-card');
+  
+    card.innerHTML = `
+      <img src="${producto.imagen}" alt="${producto.nombre}">
+      <h4>${producto.nombre}</h4>
+      <p>$${producto.precio.toLocaleString()}</p>
+      <button>¡Lo quiero!</button>
+    `;
+  
+    contenedorProductos.appendChild(card);
+  });
+  
